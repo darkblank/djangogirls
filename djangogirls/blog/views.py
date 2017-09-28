532369dc416826e3f39cf5012301542c44bfa60f
darkblank <darkblank1990@gmail.com>
2017-09-29 00:22:37 +0900

    post_add title,content 중 존재하는 값은 form에 남기기

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -29,27 +29,40 @@ def post_detail(request, pk):
 
 def post_add(request):
     # 아래의 get은 dictionary 메서드
-    if request.method == 'POST' and request.POST.get('title') and request.POST.get('content'):
-        title = request.POST['title']
-        content = request.POST['content']
+    if request.method == 'POST':
+        # request.POST(dict형 객체)에서 'title', 'content'키에 해당하는 value를 받아
+        # 새 Post객체를 생성 (save() 호출없음. 단순 인스턴스 생성)
+        # 생성한 후에는 해당 객체의 title, content를 HttpResponse로 전달
+
+        # title이나 content값이 오지 않았을 경우에는 객체를 생성하지 않고 다시 작성페이지로 이동 (render또는 redirect)
+        #   extra) 작성페이지로 이동 시 '값을 입력해주세요'라는 텍스트를 어딘가에 표시 (render)
+        #   extra*****) Bootstrap을 사용해서 modal띄우기
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        is_publish = bool(request.POST.get('is_publish'))
         author = User.objects.get(username='darkblank')
-        is_publish = bool(request.POST.get('publish'))
-        post = Post.objects.create(
-            title=title,
-            content=content,
-            author=author,
-        )
-        post_pk = post.pk
 
-        if is_publish is True:
-            post.publish()
+        # title과 content가 모두 전달되었을때만 Post생성
+        if title and content:
+            post = Post.objects.create(
+                author=author,
+                title=title,
+                content=content,
+            )
+            if is_publish:
+                post.publish()
+            else:
+                post.save()
+            return redirect('post_detail', pk=post.pk)
 
-        return redirect('post_detail', pk=post_pk)
-    else:
         context = {
-
+            'title': title,
+            'content': content,
         }
-        return render(request, 'blog/post_form.html', context)
+    else:
+        context = {}
+
+    return render(request, 'blog/post_form.html', context)
 
 
 def post_delete(request, pk):
